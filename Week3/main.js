f748175b8e7133735ce8a06edbc78617198d4935
TechWave Dynamics <techwavedy@gmail.com>
2026-04-05 19:00:28 +0530

    default

--- Week3/main.js
+++ Week3/main.js
@@ -1,5 +1,6 @@
-import { fetchUsers as fUsers, fetchCompanies } from "./util.js";
+// import { fetchUsers as fUsers, fetchCompanies } from "./util.js";
+import fetchUsers from "./util.js";
 
-const [users, companies] = await Promise.all([fUsers(), fetchCompanies()]);
+const [users] = await Promise.all([fetchUsers()]);
 console.log(users.length);
-console.log(companies.length);
+// console.log(companies.length);
--- Week3/util.js
+++ Week3/util.js
@@ -1,31 +1,45 @@
-const fetchUsers = async () => {
+// export default fetchUsers = async () => {
+//   try {
+//     const res = await fetch("https://fake-json-api.mock.beeceptor.com/users");
+//     if (!res.ok) {
+//       throw new Error("Failed");
+//     }
+//     const data = await res.json();
+//     // console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// };
+
+export default async function fetchUsers() {
   try {
     const res = await fetch("https://fake-json-api.mock.beeceptor.com/users");
     if (!res.ok) {
       throw new Error("Failed");
     }
     const data = await res.json();
-    // console.log(data);
+    console.log(data);
     return data;
   } catch (error) {
     console.error(error.message);
   }
-};
+}
 
-const fetchCompanies = async () => {
-  try {
-    const res = await fetch(
-      "https://fake-json-api.mock.beeceptor.com/companies",
-    );
-    if (!res.ok) {
-      throw new Error("Failed");
-    }
-    const data = await res.json();
-    // console.log(data);
-    return data;
-  } catch (error) {
-    console.error(error.message);
-  }
-};
+// const fetchCompanies = async () => {
+//   try {
+//     const res = await fetch(
+//       "https://fake-json-api.mock.beeceptor.com/companies",
+//     );
+//     if (!res.ok) {
+//       throw new Error("Failed");
+//     }
+//     const data = await res.json();
+//     // console.log(data);
+//     return data;
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// };
 
-export { fetchCompanies, fetchUsers };
+// export { fetchCompanies, fetchUsers };
